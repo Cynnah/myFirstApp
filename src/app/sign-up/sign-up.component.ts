dc843258b7f84e62d94f5a2a07d3137ce341e3f8
Cynnah <massot.dorian@gmail.com>
2019-04-05 12:10:56 +0200

    modification on sign-up component

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Email } from '../Email';
 
 @Component({
-  selector: 'app-sign-up',
+  selector: 'sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
 
+  Email: Email = {
+    id: 1,
+    name: 'myemail@yahoo.fr'
+  }
+
   constructor() { }
 
   ngOnInit() {
   }
-
-  email = 'myemail@yahoo.fr';
 }
